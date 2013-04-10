6c4496dc27c1d23de95a378929da7796a3789a2d
Jason DeVita <jason@wx13.com>
2013-04-10 07:59:42 -0700

    bugfix: ruby 1.9 has a different require statement for local files
    This bug only affects development, and has no impact on installed
    versions.  The jedi.rb test code used "require 'xxx'", while ruby 1.9
    seems to want "require './xxx.rb'" instead.  Ruby 1.8 seems happy with
    that.  Again, does not affect single-file or installed version.
    
    	modified:   src/jedi.rb

--- src/jedi.rb
+++ src/jedi.rb
@@ -1,11 +1,11 @@
-require 'editor'
-require 'antsy'
-require 'filebuffer'
-require 'keymap'
-require 'histories'
-require 'syntaxcolors'
-require 'bufferslist'
-require 'bufferhistory'
+require './editor.rb'
+require './antsy.rb'
+require './filebuffer.rb'
+require './keymap.rb'
+require './histories.rb'
+require './syntaxcolors.rb'
+require './bufferslist.rb'
+require './bufferhistory.rb'
 
 #---------------------------------------------------------------------
 # Run the editor
